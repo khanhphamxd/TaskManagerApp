2c548ee72bc0fec59ff3947c6bb2a62439a012c0
khanhphamxd <barrykpham267@gmail.com>
2024-09-15 16:36:00 +1000

    Revert CompletedTaskView

--- TaskManager/CompletedTaskView.swift
+++ TaskManager/CompletedTaskView.swift
@@ -11,26 +11,9 @@ struct CompletedTasksView: View {
     @ObservedObject var viewModel: TaskViewModel
 
     var body: some View {
-        VStack {
-            Text("Completed Tasks")
-                .font(.largeTitle)
-                .padding()
-
-            List {
-                ForEach(viewModel.getCompletedTasks(), id: \.id) { task in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(task.title)
-                                .font(.headline)
-                            Text(task.description)
-                                .font(.subheadline)
-                        }
-                        Spacer()
-                        Text(task.category)
-                            .font(.caption)
-                            .foregroundColor(.gray)
-                    }
-                }
+        List {
+            ForEach(viewModel.getCompletedTasks()) { task in
+                Text(task.title)
             }
         }
         .navigationTitle("Completed Tasks")
@@ -42,4 +25,5 @@ struct CompletedTasksView_Previews: PreviewProvider {
         CompletedTasksView(viewModel: TaskViewModel())
     }
 }
+
 
